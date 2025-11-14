dd045df0389d6ab3f67a6bbfb246244aa5a35d36
Paulygold <paul.weigold@gmail.com>
2025-11-14 09:22:59 +0100

    AddedSmartLogProjekt

---
+++ SmartLog/SmartLog.Biz/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartLog.Biz
+{
+    internal static class DependencyInjection
+    {
+        public static IServiceCollection AddBiz(this IServiceCollection services)
+        {
+
+            return services;
+        }
+    }
+}
